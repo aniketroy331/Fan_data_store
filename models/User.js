5c193df841fe6a50cf4f8dff5b35bbb6d4fdfe57
Aniket Roy <aniketroy10100@gmail.com>
2025-04-04 22:24:04 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -23,7 +23,6 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
@@ -36,7 +35,6 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
