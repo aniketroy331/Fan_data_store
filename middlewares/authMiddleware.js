eaf9e241d56863534ab8866368467a6749c40f8a
Aniket Roy <aniketroy10100@gmail.com>
2025-04-04 16:37:10 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, '9eee75143d722178353079853a6daf32b016546b5e0744576c6f202d2126a4d6');
     req.userId = decoded.userId;
     next();
   } catch (err) {
